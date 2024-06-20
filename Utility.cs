edddea86e559ceb6b37ce25f67c2d80e2b8e09a7
Ashutosh Kumar <kancha@lotgaming.xyz>
2024-06-20 20:37:56 +0530

    test

--- Utility.cs
+++ Utility.cs
@@ -243,6 +243,9 @@ namespace MatchZy
             PrintToAllChat($"{ChatColors.Olive}LIVE!");
             PrintToAllChat($"{ChatColors.Lime}LIVE!");
             PrintToAllChat($"{ChatColors.Green}LIVE!");
+            PrintToChatAll($"{chatPrefix} {ChatColors.Red}Plugin Version {ChatColors.Green}MatchZy v0.7.11 by -WD");
+            PrintToChatAll($"{chatPrefix} {ChatColors.Red}Commands {ChatColors.Green}!pause or !tac, !tech, !stop. Overtime enabled.");
+            PrintToChatAll($"{chatPrefix} {ChatColors.Red}Custom agent skins are prohibited. However, teams will not be disqualified. If players have an issue with that, they can ask the opponents to remove it.");
 
             // Adding timer here to make sure that CFG execution is completed till then
             AddTimer(1, () => {
@@ -632,7 +635,6 @@ namespace MatchZy
             }
             if (showCreditsOnMatchStart.Value)
             {
-                Server.PrintToChatAll($"{chatPrefix} {ChatColors.Green}MatchZy{ChatColors.Default} Plugin by {ChatColors.Green}WD-{ChatColors.Default}");
             }
         }
 
