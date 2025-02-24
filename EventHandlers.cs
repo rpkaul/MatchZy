16fc41d33b8a49040938a4add60815ad21c26696
Ashutosh Kumar <kancha@lotgaming.xyz>
2025-02-24 11:39:51 +0530

    changes

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -29,7 +29,7 @@ public partial class MatchZy
                     if (team == CsTeam.None)
                     {
                         Log($"[EventPlayerConnectFull] KICKING PLAYER STEAMID: {steamId}, Name: {player.PlayerName} (NOT ALLOWED!)");
-                        PrintToAllChat($"Kicking player {player.PlayerName} - Not a player in this game.");
+                        PrintToAllChat($"{ChatColors.Red}Kicking Player- {player.PlayerName} , SteamID {steamId}  Not Allowed Contact Admin.");
                         KickPlayer(player);
                         return HookResult.Continue;
                     }
